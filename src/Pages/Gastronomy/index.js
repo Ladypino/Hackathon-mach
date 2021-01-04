5715c5e8b924705cc4ab1261d5b2e74662a894fc
Samantha Moreno <sami@MacBook-Pro-de-Samantha.local>
2021-01-04 20:14:48 -0300

    change name home to beneficios, and create component coverpageGastronomy

--- src/Pages/Gastronomy/index.js
+++ src/Pages/Gastronomy/index.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { Fragment } from "react";
+import CoverPageGastronomy from '../../components/CoverPageGastronomy';
+
 
 const Gastronomy = () => {
-  return <div>Holaaaa Comida</div>;
+  return (
+    <Fragment>
+      <CoverPageGastronomy />
+    </Fragment>
+  )
 };
 export default Gastronomy;

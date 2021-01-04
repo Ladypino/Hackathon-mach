160d437a753194aa81bc2aa962d3c8034f08d2e2
Samantha Moreno <sami@MacBook-Pro-de-Samantha.local>
2021-01-04 17:46:02 -0300

    change name style h1 and h3

--- src/components/Coverpage.jsx
+++ src/components/Coverpage.jsx
@@ -6,8 +6,8 @@ const CoverPage = () => {
   return (
     <section>
       <div className='CoverPage--ctn'>
-        <h1>Ser #MACHLovers tiene su recompensa</h1>
-        <h3>En MACH, premiamos tu lealtad con los mejores y más exclusivos beneficios.</h3>
+        <h1 className='CoverPage--ctn-h1'>Ser #MACHLovers tiene su recompensa</h1>
+        <h3 className='CoverPage--ctn-h3'>En MACH, premiamos tu lealtad con los mejores y más exclusivos beneficios.</h3>
       </div>
       <img className='CoverPage--img' src={imageCoverPage} alt='Portada Mach Beneficios' />
       <button className='CoverPage--btn'>¡Los quiero!</button>

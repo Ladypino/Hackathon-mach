043cb51a898b3c83dabd9e59a1210e243d2fe02d
Samantha Moreno <sami@MacBook-Pro-de-Samantha.local>
2021-01-04 15:10:58 -0300

    add coverpage with letter and style

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react";
 import BenefitsResume from "../../components/BenefitsResume";
+import CoverPage from "../../components/Coverpage";
 import ProfilesResume from "../../components/ProfilesResume";
 
 const Home = () => {
   return (
-  <Fragment>
-    <BenefitsResume/>
-    <ProfilesResume/>
-  </Fragment>
+    <Fragment>
+      <CoverPage />
+      <BenefitsResume />
+      <ProfilesResume />
+    </Fragment>
   );
 };
 export default Home;

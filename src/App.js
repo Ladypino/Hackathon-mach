ab69bf5847d2c8ef01bee3ac5b123deb4dc092bf
Fabiane Santos <fanutri1@gmail.com>
2021-01-04 17:13:52 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import Routes from "./routes";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (

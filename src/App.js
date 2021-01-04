bcc1e3bce024bd52977a4f322ec6a2b981a0a709
Samantha Moreno <sami@MacBook-Pro-de-Samantha.local>
2021-01-04 11:55:03 -0300

    change name components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Routes from "./routes";
 import "./App.css";
-import Header from "./Components/Header";
+import Header from "./components/Header";
 
 function App() {
   return (

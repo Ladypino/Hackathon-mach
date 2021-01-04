60233dee5b48b16dc4ee7e8dff9e642220d1160c
Samantha Moreno <sami@MacBook-Pro-de-Samantha.local>
2021-01-04 14:24:40 -0300

    add component CoverPage

---
+++ src/components/Coverpage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import imageCoverPage from '../images/image-cover-page.jpg';
+
+const CoverPage = () => {
+    return (
+        <img src={imageCoverPage} alt='Portada Mach Beneficios' />
+    )
+};
+export default CoverPage;

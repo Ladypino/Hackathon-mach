fa4db36d65600e3d5b9dd15f00d985969e15deed
Samantha Moreno <sami@MacBook-Pro-de-Samantha.local>
2021-01-05 11:54:24 -0300

    add route gastronomy/sushiblue

--- src/components/SushiBlueGastronomy.jsx
+++ src/components/SushiBlueGastronomy.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import CoverPageGastronomy from '../components/CoverPageGastronomy';
+import '../components/style/sushiblue.css';
 
 const SushiBlueGastronomy = () => {
     return (
         <section>
-
+            <CoverPageGastronomy />
+            <div className='Gastronomy--items'>
+                {/* <img className='Gastronomy--img' src={ } alt="Sushi Blue" /> */}
+                <div className='Gastronomy--caption'>20% Sushi Blue</div>
+            </div>
         </section>
     )
 };

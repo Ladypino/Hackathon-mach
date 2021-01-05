3555b2b389e2d7a39fe9e729548e4228f8e40f41
yndira <yndinatera10@gmail.com>
2021-01-05 10:38:52 -0300

    breadcrumbs points page

--- src/components/HeroPoints.jsx
+++ src/components/HeroPoints.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 import './style/HeroPoints.css';
 
 const HeroPoints = () => {
-    return(
+    return (
         <div className="hero-points">
             <div>
+                <nav aria-label="breadcrumb">
+                    <ol className="points-breadcrumbs">
+                        <li className="points-breadcrumb-item"><a href="/home">Home</a></li>
+                        <li className="points-breadcrumb-item"><a href="/home">Beneficios</a></li>
+                        <li className="points-breadcrumb-item-active" aria-current="page"><span>Puntos</span></li>
+                    </ol>
+                </nav>
+
+                {/* <p>Home &gt; Beneficios &gt; <span>Puntos</span></p> */}
                 <h1>MACH reconoce tu valor como usuario y te premia</h1>
                 <h3>Por eso cada vez que uses nuestra app acumularás puntos que podrás canjear por lo que más te gusta</h3>
             </div>

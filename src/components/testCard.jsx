85623456b9c734f3ca97ad2ceeb276307f09e603
Fabiane Santos <fanutri1@gmail.com>
2021-01-05 22:14:08 -0300

    teste de galeria de imagenes

---
+++ src/components/testCard.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import '../components/style/testCard.css';
+
+
+
+
+const TestCard = ()=>{
+    return(
+     
+<div class="container">
+
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<h1>Pure Css Responsive Masonry Gallery</h1>
+<div class="col-md-12">
+<div class="row">
+
+
+	<div class="gal">
+	
+	<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt="" />
+	
+		<img src="https://preview.ibb.co/mWpE3Q/2.jpg" alt=""/>
+		
+			<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+			
+			<img src="https://preview.ibb.co/mysOxk/3.jpg" alt=""/>
+			
+			
+		
+			<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+				<img src="https://preview.ibb.co/mWpE3Q/2.jpg" alt=""/>
+			
+				<img src="https://preview.ibb.co/mysOxk/3.jpg" alt=""/>
+			
+				<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+				<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/><img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+				<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+				
+				<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+				<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+				<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+					<img src="https://preview.ibb.co/mysOxk/3.jpg" alt=""/>
+			
+						<img src="https://preview.ibb.co/mysOxk/3.jpg" alt=""/>
+			
+						<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+				<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+				<img src="https://preview.ibb.co/i0PmHk/1.jpg" alt=""/>
+				<img src="https://preview.ibb.co/mWpE3Q/2.jpg" alt=""/>
+		
+			
+			
+				
+	</div>
+	</div>	
+</div>
+</div>
+
+    )
+}
+
+export default TestCard;

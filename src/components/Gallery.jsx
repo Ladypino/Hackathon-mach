87e6b03a02677c588e9472cd4dbf90d40da14bf0
lady pino <ladypino112@gmail.com>
2021-01-06 14:35:09 -0300

    finish gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,12 @@
 import "./style/gallery.css";
 
-import image1 from "../images/Gallery/image1.png";
-import image2 from "../images/Gallery/image2.png";
-import image3 from "../images/Gallery/image3.png";
-import image7 from "../images/Gallery/image7.png";
-import image5 from "../images/Gallery/image5.png";
-import image6 from "../images/Gallery/image6.png";
-import image8 from "../images/Gallery/image8.gif";
+import Destacado from "../images/Gallery/Destacado.png";
+import Gastronomia from "../images/Gallery/Gastronomia.png";
+import Servicios from "../images/Gallery/Servicios.png";
+import Tienda from "../images/Gallery/Tienda.png";
+import Ofertadeldia from "../images/Gallery/Ofertadeldia.png";
+import Puntos from "../images/Gallery/Puntos.png";
+
 import { Fragment } from "react";
 // import SaleOfTheDay from "./Pages/Sale-of-the-day";
 
@@ -22,35 +22,57 @@ const GalleryViews = () => {
           Puntos MACH pensados en ti.
         </h5>
       </div>
-      <div class="container-gallery">
-        <div class="column">
-          <img className="image1" src={image1} />
-          <a href="/sale-of-the-day">
-            {" "}
-            <img className="image5" src={image5} />
-          </a>
+
+      <div class="wrapper">
+        <div class="one">
+          
+        
+         
+            <img className="image1" src={Destacado} />
+         
+          <div className='Gastronomy--caption2'>Destacados</div>
         </div>
 
-        <div class="column">
+        <div class="two">
+          
           <a href="/gastronomy">
-            {" "}
-            <img className="image2" src={image2} />
+           
+            <img className="image" src={Gastronomia} />
+            <div className='Gastronomy--caption'>Gastronomía</div>
           </a>
-          <a href="/points">
-            {" "}
-            <img className="image3" src={image3} />
-          </a>
-          <img className="image8" src={image8} />
         </div>
 
-        <div class="column">
-          <a href="/shops">
-            {" "}
-            <img className="image5" src={image7} />
-          </a>
+        <div class="three">
+          
           <a href="/mach-services">
-            {" "}
-            <img className="image6" src={image6} />
+            
+            <img className="image" src={Servicios} />
+            <div className='Gastronomy--caption2'>Servicios</div>
+          </a>
+        </div>
+        <div class="four">
+          
+          <a href="/shops">
+          
+            <img className="image" src={Tienda} />
+            <div className='Gastronomy--caption3'>Tiendas</div>
+          </a>
+        </div>
+        <div class="five">
+          
+          <a href="/sale-of-the-day">
+          
+            <img className="image" src={Ofertadeldia} />
+            <div className='Gastronomy--caption4'>Oferta del día</div>
+          </a>
+        </div>
+        <div class="six">
+          
+          
+          <a href="/points">
+          
+            <img className="image" src={Puntos} />
+            <div className='Gastronomy--caption3'>Puntos MACH</div>
           </a>
         </div>
       </div>
